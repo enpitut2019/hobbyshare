3fbbb2ab5f0162f40d379888c17bf54596c692d7
Ryo Tsuruhashi <s1711543@s.tsukuba.ac.jp>
2019-08-01 14:05:11 +0900

    Userコントローラに検索処理を追記

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -5,5 +5,15 @@ class UserController < ApplicationController
 
   def show
     @id = params[:select_user_id]
+    @selected_data = User.find(@id)
+    @queries = []
+    @queries.push(@selected_data.hobby1).push(@selected_data.hobby2).push(@selected_data.hobby3)
+    @search_results = []
+    @queries.each do |q|
+      @search_results.push(User.where(hobby1: q))
+      @search_results.push(User.where(hobby2: q))
+      @search_results.push(User.where(hobby3: q))
+    end
+    @search_results.uniq!
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   get 'user/select' => 'user#select'
+<<<<<<< Updated upstream
   post 'user/show' => 'user#show'
+=======
+  get 'user/#id/show' => 'user#show'
+>>>>>>> Stashed changes
   get 'user/home'
 end
