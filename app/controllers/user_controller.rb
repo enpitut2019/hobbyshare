957a42210839ee137ab5146df82d9c8139c93cee
Ryo Tsuruhashi <s1711543@s.tsukuba.ac.jp>
2019-08-01 15:39:19 +0900

    変更をマージ

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -12,6 +12,7 @@ class UserController < ApplicationController
     @queries = []
     #@queriesに検索クエリとなる選択されたユーザーの趣味を格納
     @queries.push(@selected_data.hobby1).push(@selected_data.hobby2).push(@selected_data.hobby3)
+
     #データベースに検索をかけて趣味が共通したユーザのデータを格納する↓
     #インスタンス配列@search_resultsの用意
     @search_results = []
@@ -19,9 +20,9 @@ class UserController < ApplicationController
     @queries.each do |q|
       #選択ユーザのidを主キーに持つデータを除外しつつ、同じ趣味をどこかに持つユーザのデータを
       #@search_resultsに格納
-      @search_results.push(User.where.not(id: @id).where(hobby1: q))
-      @search_results.push(User.where.not(id: @id).where(hobby2: q))
-      @search_results.push(User.where.not(id: @id).where(hobby3: q))
+      @search_results.push(User.where(hobby1: q).select("name","hobby1"))
+      @search_results.push(User.where(hobby2: q).select("name","hobby2"))
+      @search_results.push(User.where(hobby3: q).select("name","hobby3"))
     end
     #配列内で重複したデータを削除
     #(2種類以上が共通した場合の処理を考えるとあとあと変えたほうが良いのかも)
