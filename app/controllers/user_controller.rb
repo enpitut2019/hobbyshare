2c2a32acafda4e8de09c4fd8f5771da486f22004
Regulus495 <s1711476@s.tsukuba.ac.jp>
2019-08-02 12:36:45 +0900

    ユーザのマイページを作成しました

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -3,6 +3,9 @@ class UserController < ApplicationController
     @users = User.all
   end
 
+  def mypage
+  end
+
   def show
     #select.htmlで選択された人のidを@idに数字として格納
     @id = params[:select_user_id].to_i
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'user/select' => 'user#select'
   post 'user/show' => 'user#show'
-  get 'user/home'
+  get 'user/mypage' => 'user#mypage'
+  get '/' => 'user#mypage'
 end
