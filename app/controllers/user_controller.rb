788a760bdbf01dc3ac42e96917c4b2b7cb1a4b7b
Ryo Tsuruhashi <s1711543@s.tsukuba.ac.jp>
2019-08-02 14:20:00 +0900

    Userコントローラのコメント文を修正

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -5,16 +5,20 @@ class UserController < ApplicationController
 
   def show
     #select.htmlで選択された人のidを@idに数字として格納
-    #@id = params[:select_user_id].to_i
+    #gidにグループidを格納する
     @id = 4
     @gid = 3
-    #主キーであるidで検索し、ユーザー（自分）の趣味等のデータを検索クエリ用に取得
+    #趣味で検索するためにHobbiesの主キーであるHIDを格納する@query_hobbyidの用意
     @query_hobbyid = []
+    #UIDとHIDを結びつけているUserHobbyに対して操作中ユーザのUIDで検索をかけ、そのHIDを格納。
     UserHobby.where(user_id: @id).each do |h|
       @query_hobbyid.push(h.hobby_id)
     end
 
+    #query_guser_idに同じグループに所属するユーザのUIDを格納する
     @query_guser_id = []
+    #GIDとUIDを結びつけているGroupBelongに対して選択されたグループのGIDで検索をかけ、
+    #そのグループに所属する操作者を除くユーザのUIDを格納する。
     GroupBelong.where(group_id: @gid).each do |u|
       if u.user_id == @id
       else
@@ -22,23 +26,19 @@ class UserController < ApplicationController
       end
     end
 
-    #データベースに検索をかけて趣味が共通したユーザのデータを格納する↓
-    #インスタンス配列@results_seikeiの用意
+    #趣味が共通したユーザのデータを格納するインスタンス配列@results_seikeiの用意
     @results_seikei = []
-    #検索クエリを格納した配列の中身を用いて順に検索
+    #検索対象となった同じグループのユーザに対して順に検索を行う。
     @query_guser_id.each do |gu|
-      #選択ユーザのidを主キーに持つデータを除外しつつ、同じ趣味をどこかに持つユーザのデータを
-      #@results_seikeiに格納
+      #検索対象のユーザの持つ趣味に対して順に、操作者の持つ趣味と一致しているか判定する。
       UserHobby.where(user_id: gu).each do |uh|
         @query_hobbyid.each do |qh|
+          #一致していた場合、文として整形し配列@results_seikeiに格納する
           if qh == uh.hobby_id
             @results_seikei.push(Hobby.find(uh.hobby_id).hobby_name + "を" + User.find(gu).name + "さんと共有しています")
           end
         end
       end
     end
-    #配列内で重複したデータを削除
-    #(2種類以上が共通した場合の処理を考えるとあとあと変えたほうが良いのかも)
-
   end
 end
