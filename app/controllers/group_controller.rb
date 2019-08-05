d3fae7d85f67f405ea596130798956a86def701f
Regulus495 <s1711476@s.tsukuba.ac.jp>
2019-08-05 10:13:28 +0900

    グループ内のメンバを一覧表示できるようにしました。

--- app/controllers/group_controller.rb
+++ app/controllers/group_controller.rb
@@ -1,4 +1,24 @@
 class GroupController < ApplicationController
   def list
+    #select.htmlで選択された人のidを@idに数字として格納
+    #gidにグループidを格納する
+    @id = params[:user_id].to_i
+    @gid = params[:group_id].to_i
+
+    #query_guser_idに同じグループに所属するユーザのUIDを格納する
+    @query_guser_id = []
+    #GIDとUIDを結びつけているGroupBelongに対して選択されたグループのGIDで検索をかけ、
+    #そのグループに所属する操作者を除くユーザのUIDを格納する。
+    GroupBelong.where(group_id: @gid).each do |u|
+      if u.user_id == @id
+      else
+      @query_guser_id.push(u.user_id)
+      end
+    end
+    @group_user_all = []
+    @query_guser_id.each do |qgi|
+      @group_user_all.push(User.find(qgi).name)
+    end
+
   end
 end
