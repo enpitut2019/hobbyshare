5a56c67f85bcdfd0751c8acbf2d56c0e8c362ed8
Ryo Tsuruhashi <s1711543@s.tsukuba.ac.jp>
2019-10-23 14:04:42 +0900

    account_controllerにaccountのmypageへ遷移する用の処理を追記

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -4,6 +4,47 @@ class AccountController < ApplicationController
   end
 
   def mypage
+
+    #現状account名は不変でUser名はいじれる感じなのでaccount名とユーザ名の表示は考えたほうがいいかも
+    @account_id = params[:account_id].to_i
+    #account名を格納
+    @account_name = Account.find_by(id: @account_id).name
+    @user_id = Account.find_by(id: @account_id).user_id
+    #ここにSessionを入れる予定
+    #
+    #
+
+    #よしなにポイント
+    @belong_record = GroupBelong.where(user_id: @user_id)
+
+    #accountsではグループ所属が0の場合あり
+    if @belong_record.empty?
+
+    else
+      @belong = []
+      @belong_record.each do |record|
+        @belong.push(record.group_id)
+      end
+      #グループIDに対応するレコードを取ってくる
+      @belong_group_name = []
+      @belong.each do |gid|
+        @belong_group_name.push(Group.find_by(id: gid))
+      end
+    end
+    #ユーザ名(Model:User)を変数に格納
+    @user_name = User.find_by(id: @user_id).name
+    #ユーザの趣味を取得して変数に入れる
+    @uhobby_record = UserHobby.where(user_id: @user_id)
+    #uhobby_recordからhobbyIDだけを取り出して配列にする
+    @hobbies_id = []
+    @uhobby_record.each do |record|
+      @hobbies_id.push(record.hobby_id)
+    end
+    #HobbyIDに対応するレコードを取ってくる
+    @users_hobbies = []
+    @hobbies_id.each do |hid|
+      @users_hobbies.push(Hobby.find_by(id: hid))
+    end
   end
 
   def new_account
@@ -13,4 +54,6 @@ class AccountController < ApplicationController
      flash[:notice] = "アカウントを作成しました！"
      redirect_to("/account/#{ac.id}")
   end
+
+
 end
