f02496a7ddaa6453fb66e394f458aa3517fe9d74
Regulus495 <s1711476@s.tsukuba.ac.jp>
2019-10-30 13:08:46 +0900

    アカウント作成入力フォームとアカウント作成処理のリファクタリング

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -7,8 +7,13 @@ class AccountController < ApplicationController
   end
 
   def new_account
+    if Account.find_by(name: params[:name])
+      flash[:notice] = "その名前はすでに使われています"#nameが既存のものと一致した場合は弾く
+      redirect_to("/account/sign_up")
+    else
      ac = Account.create(name: params[:name], mail: params[:mail], password: params[:password])
      flash[:notice] = "アカウントを作成しました！"
      redirect_to("/account/#{ac.id}")
+   end
   end
 end
