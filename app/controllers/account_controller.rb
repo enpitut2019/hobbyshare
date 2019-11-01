b686cf6fe56c61d6c901d9ca5d3d051b72eb08c2
sekitaku <sekitaku56@gmail.com>
2019-11-01 16:01:07 +0900

    アカウント作成処理にセッションを使用

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -9,14 +9,47 @@ class AccountController < ApplicationController
   end
 
   def new_account
+    session_id = session[:login_account_id]
     if Account.find_by(name: params[:name])
       flash[:notice] = "その名前はすでに使われています"#nameが既存のものと一致した場合は弾く
       redirect_to("/account/sign_up")
-    else
-     ac = Account.create(name: params[:name], mail: params[:mail], password: params[:password])
+    end
+
+    # セッションがない場合
+    if session_id == nil
+      # アカウントを作成
+     ac = Account.create(name: params[:name], password: params[:password])
+      # dummy_userを設定
+     dummy_user = User.create()
+     ac.user_id = dummy_user.id
+     # セッションを書き換え
+     session[:login_account_id] = ac.id
+     # マイページへ飛ばす
      flash[:notice] = "アカウントを作成しました！"
      redirect_to("/account/#{ac.id}")
-   end
+    else
+      # セッションがあるのでログインしているアカウントを取り出す
+      login_account = Account.find_by(id:session_id)
+      # 既に別のアカウントでログインしている場合
+      if login_account.is_temp == false
+        # トップページへ戻す
+        flash[:notice] = "既に別のアカウントでログインしています！"
+        redirect_to("/")
+      # ログインしているのは仮アカウントだった場合
+      else
+        # name,passwordの設定
+        login_account.name = params[:name]
+        login_account.password = params[:password]
+        # dummy_userの作成
+        dummy_user = User.create()
+        login_account.user_id = dummy_user.id
+        # 仮アカウントから本アカウント扱いに変更
+        login_account.is_temp = false
+        # マイページへリダイレクト
+        flash[:notice] = "アカウントを作成しました！"
+        redirect_to("/account/#{login_account.id}")
+      end
+    end
   end
 
   def login_process#セッションはまかせた
