91c0feb2349e346ecdbc55e0940539dd36205bfb
sekitaku <sekitaku56@gmail.com>
2019-11-08 17:02:02 +0900

    ヘッダを動的に変化

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+  before_action :session_check
+
+  def session_check
+    @is_session_exists = false
+    if session[:login_account_id] == nil
+      @is_session_exists = false
+      return
+    end
+
+    login_account = Account.find_by(id:session[:login_account_id])
+    if login_account.is_temp == true
+      @is_session_exists = false
+    else
+      @is_session_exists = true
+    end
+  end
 end
